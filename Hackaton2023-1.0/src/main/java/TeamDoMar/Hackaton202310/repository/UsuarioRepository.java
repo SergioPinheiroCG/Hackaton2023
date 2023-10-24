78b28976a755a3c27c50c505024ab224f528d323
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2023-10-24 18:33:48 -0300

    Iniciando Branch

--- Hackaton2023-1.0/src/main/java/TeamDoMar/Hackaton202310/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package TeamDoMar.Hackaton202310.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import TeamDoMar.Hackaton202310.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
-}
